d7a2c1991661fecb543f28f29dd6570e41f628a1
Robson <robson.080490@gmail.com>
2024-03-28 16:18:53 -0300

    teste.

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,12 @@
 package com.devsuperior.dsmeta.repositories;
 
+import com.devsuperior.dsmeta.dto.SaleMinDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsmeta.entities.Sale;
 
-public interface SaleRepository extends JpaRepository<Sale, Long> {
+import java.util.List;
 
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+    List<SaleMinDTO> teste();
 }
